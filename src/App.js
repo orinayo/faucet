20bb4916f7a304c729326cf35c388914f2419304
Oluwasegun Orinayo Oyelade <orinayooyelade@gmail.com>
2022-08-21 13:40:45 +0200

    fix webpack 5 errors. inject provider from metamask

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import "./App.css";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null;
+      // with metamask we have access to window.ethereum & window.web3
+      // metamask injects these global APIs into the website
+      // we can then request users, accounts, write data to blockchain,
+      // sign messages and transactions
+      if (window.ethereum) {
+        provider = window.ethereum;
+        try {
+          await provider.enable()
+        } catch (error) {
+          console.error(error)
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider,
+      });
+    };
+
+    loadProvider();
+  }, []);
+
   return (
     <>
       <div className="faucet-wrapper">
